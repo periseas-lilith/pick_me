cbd52605d57390d5795655c07a7dc9a437a9493b
tayde-castillo <ediat_ilera@hotmail.com>
2020-12-02 00:00:07 -0600

    change logo auth

---
+++ resources/views/vendor/jetstream/components/application-logo.blade.php
@@ -0,0 +1,6 @@
+<div class="text-center" style="max-width: 120px;">
+    <img src="{{ asset('img/logo.png') }}" width="100%" />
+</div>
+<div class="text-center">
+    <h1 style="font-weight: 600; font-size: 2rem;">Park ME</h1>
+</div>
